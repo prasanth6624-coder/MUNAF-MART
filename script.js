a28b5dbe235ca2d5322162cf15008f69a957d798
prasanth6624-coder <prasanth6624@icloud.com>
2026-07-29 00:13:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
 
+searchInput.addEventListener("keyup", function () {
+    const filter = searchInput.value.toLowerCase();
+
+    cards.forEach(function (card) {
+        const product = card.querySelector("h2").textContent.toLowerCase();
+
+        if (product.includes(filter)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
