ff1222a622287437a2422262dfaaefb7a5eaf8a5
prasanth6624-coder <prasanth6624@icloud.com>
2026-07-29 23:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const searchInput = document.getElementById("searchBox");
-const cards = document.querySelectorAll(".card");
-
-searchInput.addEventListener("keyup", function () {
-    const filter = searchInput.value.toLowerCase();
-
-    cards.forEach(function (card) {
-        const product = card.querySelector("h2").textContent.toLowerCase();
-
-        if (product.includes(filter)) {
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
-    });
-});
 let cart = [];
 
 function addToCart(productName, price) {
@@ -27,7 +11,7 @@ function addToCart(productName, price) {
     let list = "";
 
     cart.forEach(function(item) {
-        list += "<p>" + item.name + " - Rs." + item.price + "</p>";
+        list += `<p>${item.name} - Rs.${item.price}</p>`;
     });
 
     document.getElementById("cartItems").innerHTML = list;
